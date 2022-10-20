59355c74a01dd4389bc822d1d09191ca0514813f
Felipe Mendes <felipemadruga@gmail.com>
2022-10-20 15:55:15 -0300

    Conclui exercicio 05

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/ex-05.js
@@ -0,0 +1,8 @@
+let weekDay = "juma";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+} else if (weekDay === "sabado" || weekDay === "domingo") {
+    console.log("FINALMENTE, descanso merecido UwU”")
+} else { console.log("Desculpe, mas esse dia não existe...")}
+
