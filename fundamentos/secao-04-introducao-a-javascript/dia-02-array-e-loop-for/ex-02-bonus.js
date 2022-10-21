d1773ac50c957bf3071b60a6f99c52a6ee41edcc
Felipe Mendes <felipemadruga@gmail.com>
2022-10-21 18:23:52 -0300

    Conclui exercicio bonus

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/ex-02-bonus.js
@@ -0,0 +1,54 @@
+// 1- Ordene o array numbers em ordem crescente e imprima seus valores;
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let i = 1; i < numbers.length; i += 1) {
+
+//     for (let j = 0; j < i; j += 1) {
+
+//       if (numbers[i] < numbers[j]) {
+//         let position = numbers[i];
+//         numbers[i] = numbers[j];
+//         numbers[j] = position;
+//       }
+//     }
+
+//   }
+
+//   console.log(numbers)
+
+// 2- Ordene o array numbers em ordem decrescente e imprima seus valores;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let i = 1; i < numbers.length; i += 1) {
+
+//     for (let j = 0; j < i; j += 1) {
+
+//       if (numbers[i] > numbers[j]) {
+//         let position = numbers[i];
+//         numbers[i] = numbers[j];
+//         numbers[j] = position;
+//       }
+//     }
+
+//   }
+
+//   console.log(numbers)
+
+  // 3- Agora, crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
+
+  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  let newArr = [];
+
+  for (let i=0; i<numbers.length; i += 1) {
+    if (i !== numbers.length - 1) {
+    newArr.push(numbers[i] * numbers[i+1]);
+    } else {
+    newArr.push(numbers[i] * 2);
+    }    
+  }
+
+  console.log(newArr)
