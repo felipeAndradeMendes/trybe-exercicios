6bd2518315cfc18baa2712c5da8973b4d613596b
Felipe Mendes <felipemadruga@gmail.com>
2022-10-25 16:15:37 -0300

    Conclui exercicio numeros primos

--- fundamentos/secao-04introducao-a-javascript/dia-03-logica-de-programacao/ex-04.js
+++ fundamentos/secao-04introducao-a-javascript/dia-03-logica-de-programacao/ex-04.js
@@ -1,8 +1,18 @@
 // Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.
 
-for (let i = 50; i >= 0; i -= 1) {
-    
-    for (let j = 2; i <= j*j; j += 1) {
-        if(i % j === 0)
-    }
+let maiorPrimo = 0;
+
+for (let numero = 2; numero <= 50; numero += 1) {
+  let isPrime = true;
+
+  for (let divisor = 2; divisor < numero; divisor += 1) {
+    if (numero % divisor === 0) {
+      isPrime = false;
+    }    
+  }  
+  if(isPrime) {
+    maiorPrimo = numero;
+  }
 }
+
+console.log(maiorPrimo)
