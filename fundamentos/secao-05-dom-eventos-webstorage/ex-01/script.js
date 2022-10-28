fdfcda2cf48db34c6a155f87bd25b7adbcdf5ed7
Felipe Mendes <felipemadruga@gmail.com>
2022-10-28 15:05:13 -0300

    Conclui requisito 01 do exercicio 01 pos aula

---
+++ fundamentos/secao-05-dom-eventos-webstorage/ex-01/script.js
@@ -0,0 +1,15 @@
+// 1. Crie e execute uma função que mude o texto na tag `<p>-----</p>`, para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+
+const mudaTextoParagrafo = () => {
+    const paragrafo = document.getElementsByTagName('p');
+
+    texto = 'Daqui a odia anome imagino trabalhando na área muito feliz.'
+    return paragrafo[1].innerText = texto;
+}
+
+// console.log(mudaTextoParagrafo());
+// mudaTextoParagrafo()
+
+
+// 2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+
