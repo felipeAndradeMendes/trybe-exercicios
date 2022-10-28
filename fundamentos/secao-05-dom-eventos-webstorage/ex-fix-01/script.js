c4f34a4bf26f5c5d1f6e8962a66cd5e401b7cd6e
Felipe Mendes <felipemadruga@gmail.com>
2022-10-28 10:15:25 -0300

    Conclui exercicio de fixação 01

---
+++ fundamentos/secao-05-dom-eventos-webstorage/ex-fix-01/script.js
@@ -0,0 +1,12 @@
+const title = document.getElementById('page-title');
+const secParagraph = document.getElementById('second-paragraph');
+const subtitulo = document.getElementById('subtitle');
+
+title.style.color = 'red';
+title.innerText = 'Juma';
+
+secParagraph.style.fontSize = '2em';
+
+subtitulo.style.fontFamily = 'arial';
+subtitulo.style.fontSize = '3em';
+subtitulo.innerHTML = 'Bundalelê tá doente?';
