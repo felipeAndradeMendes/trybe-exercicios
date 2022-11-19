b33cc72ecc0024d1a67553ee211aecc6b48827ea
Felipe Mendes <felipemadruga@gmail.com>
2022-11-19 12:04:45 -0300

    Conclui exercício de fixação 05 - dia 01

---
+++ fundamentos/secao-07-intro-es06-e-testes/dia-01/ex-fix-05/script.js
@@ -0,0 +1,72 @@
+// const person = {
+//   name: 'Alberto',
+//   lastName: 'Gomes',
+//   age: 20,
+// };
+
+// const info = {
+//   age: 23,
+//   job: 'engenheiro',
+// };
+
+// const family = {
+//   children: ['Maria', 'João'],
+//   wife: 'Ana',
+// };
+
+// Object.assign(person, info, family)
+// console.log(person)
+
+///////////////////////////
+
+// const person = {
+//   name: 'Roberto',
+// };
+
+// const lastName = {
+//   lastName: 'Silva',
+// };
+
+// const clone = Object.assign(person, lastName);
+
+// console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+// console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+// //o objeto retornado pelo método Object.assign é o próprio objeto destino, que foi alterado adicionando as novas propriedades.
+
+
+
+// clone.name = 'Maria';
+
+// console.log('Mudando a propriedade name através do objeto clone')
+// console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+// console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+// console.log('--------------');
+
+// person.lastName = 'Ferreira';
+
+// console.log('Mudando a propriedade lastName através do objeto person');
+// console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+// console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
+
+
+
+///////////////////////////////////
+
+
+const person = {
+  name:'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({},person,lastName);
+console.log(newPerson);
+
+newPerson.name = 'Gilberto';
+console.log(newPerson);
+console.log(person);
+
+
