86efc32572478a7d4ca7aaaaa9f667bf5a2156c3
Felipe Mendes <felipemadruga@gmail.com>
2022-11-30 15:53:38 -0300

    Conclui exercício de fixação 03 - dia 02

---
+++ fundamentos/secao-08-HOFs-JS-ES6/dia-02/ex-fix-03/script.js
@@ -0,0 +1,103 @@
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: '59' },
+      { name: 'Português', nota: '80' },
+      { name: 'Química', nota: '78' },
+      { name: 'Biologia', nota: '92' },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '76' },
+      { name: 'Português', nota: '90' },
+      { name: 'Química', nota: '70' },
+      { name: 'Biologia', nota: '80' },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '91' },
+      { name: 'Português', nota: '85' },
+      { name: 'Química', nota: '92' },
+      { name: 'Biologia', nota: '90' },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '70' },
+      { name: 'Português', nota: '70' },
+      { name: 'Química', nota: '60' },
+      { name: 'Biologia', nota: '50' },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '80' },
+      { name: 'Português', nota: '82' },
+      { name: 'Química', nota: '79' },
+      { name: 'Biologia', nota: '75' },
+    ],
+  },
+];
+
+// Função para buscar e imprimir o nome completo de todos os estudantes que estudam no turno da manhã.
+
+// Usando map e filter:
+
+const estudantesManha = () => {
+  return estudantes.filter(estudante => estudante.turno === 'Manhã').map(estudante => `${estudante.nome} ${estudante.sobrenome}`);
+};  
+
+console.log(estudantesManha());
+
+
+
+/// Buscar um estudante pelo nome e retornar a situação dele em cada matéria:
+// Usando find e map;
+
+const findStudent = (array, student) => {
+  const studentFound = array.find((element) => element.nome === student);
+  const studentMaterias = studentFound.materias;
+
+  const situacaoMaterias = studentMaterias.map((element) => `Matéria: ${element.name} / Nota: ${element.nota} - ${element.nota >= 60 ? 'Aprovado' : 'Reporvado'} `);
+
+  return situacaoMaterias;
+};
+
+console.log(findStudent(estudantes, 'Natalia'));
+
+
+
