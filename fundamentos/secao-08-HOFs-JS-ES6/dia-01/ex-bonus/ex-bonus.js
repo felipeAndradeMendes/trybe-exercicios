e61bb7ecf983f97b6cb9a96ea080b47f43789fed
Felipe Mendes <felipemadruga@gmail.com>
2022-12-04 09:54:26 -0300

    Conclui exercício bonus pt 01_01 - dia 01

--- fundamentos/secao-08-HOFs-JS-ES6/dia-01/ex-bonus/ex-bonus.js
+++ fundamentos/secao-08-HOFs-JS-ES6/dia-01/ex-bonus/ex-bonus.js
@@ -24,3 +24,15 @@ const battleMembers = { mage, warrior, dragon };
 // Crie uma função que retorne o dano do dragão.
 
 // O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+
+const dragonDamage = () => {
+  const minDamage = 15;
+  const damage = Math.floor(Math.random() * (dragon.strength - minDamage + 1)) + minDamage;
+  return damage;
+};
+// console.log(dragonDamage());
+
+
+
+// Crie uma função que retorne o dano causado pelo warrior.
+// O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
