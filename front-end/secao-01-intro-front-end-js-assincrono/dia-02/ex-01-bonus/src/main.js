07729363a6d217ef876d6ecc347538f78c72c865
Felipe Mendes <felipemadruga@gmail.com>
2023-01-15 20:00:25 -0300

    Finaliza exercicio bonus - dia 02

--- front-end/secao-01-intro-front-end-js-assincrono/dia-02/ex-01-bonus/src/main.js
+++ front-end/secao-01-intro-front-end-js-assincrono/dia-02/ex-01-bonus/src/main.js
@@ -1,1 +1,34 @@
 import './style.css';
+
+const btnDog = document.querySelector('#btn-dog');
+const btnCat = document.querySelector('#btn-cat');
+const btnSurprise = document.querySelector('#btn-surprise');
+const petImg = document.querySelector('#pet-img');
+
+const getRandomDog = async () => {
+  const resolve = await fetch('https://dog.ceo/api/breeds/image/random');
+  const data = await resolve.json();
+  petImg.src = data.message;
+};  
+btnDog.addEventListener('click', getRandomDog);
+
+const getRandomCat = async () => {
+  const resolve = await fetch('https://aws.random.cat/meow');
+  const data = await resolve.json();
+  petImg.src = data.file;
+};
+btnCat.addEventListener('click', getRandomCat) ;
+
+const surpriseMe = async () => {
+
+  Promise.any([
+    fetch('https://dog.ceo/api/breeds/image/random'),
+    fetch('https://aws.random.cat/meow')
+  ])
+  .then(result => result.json())
+  .then((data) => {
+    const imgUrl = data.message || data.file;
+    petImg.src = imgUrl;
+  });
+};
+btnSurprise.addEventListener('click', surpriseMe);
