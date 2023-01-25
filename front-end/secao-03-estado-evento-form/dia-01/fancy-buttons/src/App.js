924db1286870b84f900b2cb2f406f4ab90c434fd
Felipe Mendes <felipemadruga@gmail.com>
2023-01-25 19:25:59 -0300

    Conclui exercicio de fixação 04

--- front-end/secao-03-estado-evento-form/dia-01/fancy-buttons/src/App.js
+++ front-end/secao-03-estado-evento-form/dia-01/fancy-buttons/src/App.js
@@ -18,37 +18,45 @@ class App extends React.Component {
   }
 
   clickBtn01 = () => {
-    // console.log(`Clicou no botão 01`)   
-    // console.log(this);
     this.setState((estadoAnterior, _props) => ({
       cliquesBtn01: estadoAnterior.cliquesBtn01 + 1,
-    }));
-    // console.log(this.state.cliquesBtn01)
+    }), () => {
+      console.log(`Clicou no botão 1: ${this.testBtnColor(this.state.cliquesBtn01)}`);
+    })     
   }
 
   clickBtn02 = () => {
-    // console.log(`Clicou no botão 02`)
     this.setState((estadoAnterior, _props) => ({
       cliquesBtn02: estadoAnterior.cliquesBtn02 + 1,
-    }));
+    }), () => {
+      console.log(`Clicou no botão 2: ${this.testBtnColor(this.state.cliquesBtn02)}`);
+    });
+
   }
+
   
   clickBtn03 = () => {
-    // console.log(`Clicou no botão 03`)
     this.setState((estadoAnterior, _props) => ({
       cliquesBtn03: estadoAnterior.cliquesBtn03 + 1,
-    }));
+    }), () => {
+      console.log(`Clicou no botão 3: ${this.testBtnColor(this.state.cliquesBtn03)}`);
+    })
+  }
+
+  testBtnColor = (num) => {
+    return num % 2 === 0 ? 'green' : 'white';
   }
 
   render() {
+    const { cliquesBtn01, cliquesBtn02, cliquesBtn03 } = this.state;
     return (
       <div>
-        <button onClick={ this.clickBtn01 }>Botão 01</button>
-        <button onClick={ this.clickBtn02 }>Botão 02</button>
-        <button onClick={ this.clickBtn03 }>Botão 03</button>
-        <p>Cliques no Botão 01 = {this.state.cliquesBtn01}  </p>
-        <p>Cliques no Botão 02 = {this.state.cliquesBtn02}  </p>
-        <p>Cliques no Botão 03 = {this.state.cliquesBtn03}  </p>
+        <button style={ {backgroundColor: this.testBtnColor(cliquesBtn01)} } onClick={ this.clickBtn01 }>Botão 01</button>
+        <button style={ {backgroundColor: this.testBtnColor(cliquesBtn02)} } onClick={ this.clickBtn02 }>Botão 02</button>
+        <button style={ {backgroundColor: this.testBtnColor(cliquesBtn03)} } onClick={ this.clickBtn03 }>Botão 03</button>
+        <p>Cliques no Botão 02 = {cliquesBtn02}  </p>
+        <p>Cliques no Botão 01 = {cliquesBtn01}  </p>
+        <p>Cliques no Botão 03 = {cliquesBtn03}  </p>
       </div>
       
     );
