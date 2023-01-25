3a91e35f238b10969943cb2113a4ba5feb765587
Felipe Mendes <felipemadruga@gmail.com>
2023-01-25 18:05:40 -0300

    Finaliza exercicio de fixação 03

--- front-end/secao-03-estado-evento-form/dia-01/fancy-buttons/src/App.js
+++ front-end/secao-03-estado-evento-form/dia-01/fancy-buttons/src/App.js
@@ -3,29 +3,41 @@ import './App.css';
 
 
 
-function clickBtn02() {
-  console.log(`Clicou no botão 02`)
-}
 
-function clickBtn03() {
-  console.log(`Clicou no botão 03`)
-}
 class App extends React.Component {
-  constructor() {
-    super()
-    this.clickBtn01 = this.clickBtn01.bind(this)
-  }
+
+  // constructor() {
+  //   super()
+  //   this.clickBtn01 = this.clickBtn01.bind(this);
+  // }
   
-  clickBtn01() {
-    console.log(`Clicou no botão 01`)
+  state = {
+    cliquesBtn01: 0,
+    cliquesBtn02: 0,
+    cliquesBtn03: 0,
+  }
+
+  clickBtn01 = () => {
+    // console.log(`Clicou no botão 01`)   
+    // console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      cliquesBtn01: estadoAnterior.cliquesBtn01 + 1,
+    }));
+    // console.log(this.state.cliquesBtn01)
   }
 
   clickBtn02 = () => {
-    console.log(`Clicou no botão 02`)
+    // console.log(`Clicou no botão 02`)
+    this.setState((estadoAnterior, _props) => ({
+      cliquesBtn02: estadoAnterior.cliquesBtn02 + 1,
+    }));
   }
   
   clickBtn03 = () => {
-    console.log(`Clicou no botão 03`)
+    // console.log(`Clicou no botão 03`)
+    this.setState((estadoAnterior, _props) => ({
+      cliquesBtn03: estadoAnterior.cliquesBtn03 + 1,
+    }));
   }
 
   render() {
@@ -34,6 +46,9 @@ class App extends React.Component {
         <button onClick={ this.clickBtn01 }>Botão 01</button>
         <button onClick={ this.clickBtn02 }>Botão 02</button>
         <button onClick={ this.clickBtn03 }>Botão 03</button>
+        <p>Cliques no Botão 01 = {this.state.cliquesBtn01}  </p>
+        <p>Cliques no Botão 02 = {this.state.cliquesBtn02}  </p>
+        <p>Cliques no Botão 03 = {this.state.cliquesBtn03}  </p>
       </div>
       
     );
