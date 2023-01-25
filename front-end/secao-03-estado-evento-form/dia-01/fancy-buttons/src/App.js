9b2f8c60ff7040042adf7059692707e0670d6959
Felipe Mendes <felipemadruga@gmail.com>
2023-01-25 14:25:35 -0300

    Conclui exercício de fixação 02

--- front-end/secao-03-estado-evento-form/dia-01/fancy-buttons/src/App.js
+++ front-end/secao-03-estado-evento-form/dia-01/fancy-buttons/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function clickBtn01() {
-  console.log(`Clicou no botão 01`)
-}
+
 
 function clickBtn02() {
   console.log(`Clicou no botão 02`)
@@ -13,12 +11,29 @@ function clickBtn03() {
   console.log(`Clicou no botão 03`)
 }
 class App extends React.Component {
+  constructor() {
+    super()
+    this.clickBtn01 = this.clickBtn01.bind(this)
+  }
+  
+  clickBtn01() {
+    console.log(`Clicou no botão 01`)
+  }
+
+  clickBtn02 = () => {
+    console.log(`Clicou no botão 02`)
+  }
+  
+  clickBtn03 = () => {
+    console.log(`Clicou no botão 03`)
+  }
+
   render() {
     return (
       <div>
-        <button onClick={ clickBtn01 }>Botão 01</button>
-        <button onClick={ clickBtn02 }>Botão 02</button>
-        <button onClick={ clickBtn03 }>Botão 03</button>
+        <button onClick={ this.clickBtn01 }>Botão 01</button>
+        <button onClick={ this.clickBtn02 }>Botão 02</button>
+        <button onClick={ this.clickBtn03 }>Botão 03</button>
       </div>
       
     );
