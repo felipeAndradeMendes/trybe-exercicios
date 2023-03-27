790b816c66d56b09dbacd3a9e2ca16e8a272cfae
Felipe Mendes <felipemadruga@gmail.com>
2023-03-27 11:27:15 -0300

    Conclui exercício de fixação 01

--- frontend/dia-03/ex-fix-01/src/App.js
+++ frontend/dia-03/ex-fix-01/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+// import Swal from 'sweetalert2';
+import useFormInput from './hooks/useFormInput';
 import './App.css';
 
 function App() {
+  const firstName = useFormInput('');
+  const lastName = useFormInput('');
+  const email = useFormInput('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // Swal.fire(
+    //   'Formulário enviado',
+    //   JSON.stringify({
+    //     firstName: firstName.value,
+    //     lastName: lastName.value,
+    //     email: email.value,
+    //   }),
+    //   'success'
+    // );
+    firstName.clear();
+    lastName.clear();
+    email.clear();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <label>
+          First name:
+          <input value={firstName.value} onChange={firstName.onChange} />
+        </label>
+        <label>
+          Last name:
+          <input value={firstName.value} onChange={firstName.onChange} />
+        </label>
+        <label>
+          E-mail:
+          <input value={email.value} onChange={email.onChange} />
+        </label>
+        <button type="submit">Submeter formulário </button>
+      </form>
     </div>
   );
 }
