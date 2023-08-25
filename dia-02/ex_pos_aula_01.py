987e80fb51915272e66be8cb32ed65b6883a2fec
Felipe Mendes <felipemadruga@gmail.com>
2023-08-25 15:49:11 -0300

    Conclui ex pos aula 01 - dia 02

--- dia-02/ex_pos_aula_01.py
+++ dia-02/ex_pos_aula_01.py
@@ -1,17 +1,14 @@
-from array import array
-
-########## ex 01
+# ex 01
 """
 Em um software monitor, o qual verifica a resiliência de outro software,
 precisamos saber o tempo máximo que um software permaneceu sem instabilidades.
 Para isto, a cada hora é feito uma requisição ao sistema
 para verificamos se está tudo bem.
-Supondo um array que contenha os estados coletados por nosso software, 
+Supondo um array que contenha os estados coletados por nosso software,
 calcule quanto tempo máximo que o servidor permaneceu sem instabilidades.
 """
 
 
-# Solução usando um for e dicionario
 valores_coletados_1 = [0, 1, 1, 1, 0, 0, 1, 1]  # 3
 valores_coletados_2 = [1, 1, 1, 1, 0, 0, 1, 1]  # 4
 
@@ -30,6 +27,7 @@ def max_stability(collected_values):
     return max_period
 
 
+print(max_stability(valores_coletados_1))
 print(max_stability(valores_coletados_2))
 
 
@@ -46,4 +44,5 @@ def verify_max_time_ok(collected_values):
     return max_time
 
 
+print(max_stability(valores_coletados_1))
 print(verify_max_time_ok(valores_coletados_2))
